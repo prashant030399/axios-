a6dc2d2dc12f7488d9042aacf0e83b2e8c367b4d
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-08-20 12:03:46 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
+// import { useState } from "react";
+// import ReactDOM from "react-dom/client";
+// import Todos from "./Todos";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+//   const [todos, setTodos] = useState([]);
+
+//   const increment = () => {
+//     setCount((c) => c + 1);
+//   };
+//   const addTodo = () => {
+//     setTodos((t) => [...t, "New Todo"]);
+//   };
+
+//   return (
+//     <>
+//       <Todos todos={todos} addTodo={addTodo} />
+//       <hr />
+//       <div>
+//         Count: {count}
+//         <button onClick={increment}>+</button>
+//       </div>
+//     </>
+//   );
+// };
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from "axios";
 
 
 function Data(){
-    axios.get("https://jsonplaceholder.typicode.com/posts").
+    axios.get("https://jsonplaceholder.typicode.com/comments").
     then(res => console.log(res.data));
 }
-
+        
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
     <Data/>
 );
-
-
-
-
